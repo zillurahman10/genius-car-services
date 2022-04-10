e863de5b8e47407c2a0cccca7b39bd35d9f251d0
zillurahman10 <raihan.brac2019@gmail.com>
2022-04-11 02:08:40 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom';
 import About from './Pages/About/About'
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import Header from ''
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
