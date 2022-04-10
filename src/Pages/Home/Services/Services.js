29a41842511ad74381a5e4221c045f45122e0e59
zillurahman10 <raihan.brac2019@gmail.com>
2022-04-11 02:07:27 +0600

    first

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h2>This is about</h2>
+        </div>
+    );
+};
+
+export default Services;
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+const firebaseConfig = {
+    apiKey: "AIzaSyCFfVBDdM6kpX-Z4uuWAppxpgRlId7Jum4",
+    authDomain: "genius-car-services-a4e92.firebaseapp.com",
+    projectId: "genius-car-services-a4e92",
+    storageBucket: "genius-car-services-a4e92.appspot.com",
+    messagingSenderId: "78431438545",
+    appId: "1:78431438545:web:58f6ec144a8d517ba6a7f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app)
+
+export default auth;
